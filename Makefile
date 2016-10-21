325a70d601a43b8984d086ecc6f78f09de74f9a9
Werner Van Geit <werner.vangeit@epfl.ch>
2016-10-21 15:41:56 +0200

    Remove example directory, license

--- Makefile
+++ Makefile
@@ -2,7 +2,4 @@ all: install
 install:
 	pip install -q . --upgrade
 test: install clean
-	cd examples/test_run; bluepymm ./mm_conf.json
 clean:
-	rm -rf examples/test_run/tmp;
-	rm -f examples/test_run/scores.sqlite;
--- setup.py
+++ setup.py
@@ -1,25 +1,6 @@
 #!/usr/bin/env python
 """bluepymm  setup """
 
-"""
-Copyright (c) 2016, EPFL/Blue Brain Project
-
- This file is part of BluePyOpt <https://github.com/BlueBrain/BluePyOpt>
-
- This library is free software; you can redistribute it and/or modify it under
- the terms of the GNU Lesser General Public License version 3.0 as published
- by the Free Software Foundation.
-
- This library is distributed in the hope that it will be useful, but WITHOUT
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
- details.
-
- You should have received a copy of the GNU Lesser General Public License
- along with this library; if not, write to the Free Software Foundation, Inc.,
- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-"""
-
 import setuptools
 
 setuptools.setup(
@@ -31,7 +12,6 @@ setuptools.setup(
     author_email="werner.vangeit@epfl.ch",
     description="Model Management Python Library (bluepymm)",
     long_description="Model Management Python Library (bluepymm)",
-    license="LGPLv3",
     keywords=(
         'optimisation',
         'neuroscience',
@@ -39,8 +19,6 @@ setuptools.setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
-        'License :: OSI Approved :: GNU Lesser General Public '
-        'License v3 (LGPLv3)',
         'Programming Language :: Python :: 2.7',
         'Operating System :: POSIX',
         'Topic :: Scientific/Engineering',
