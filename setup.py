04abd33e2e755fb0d885c9fa1c38c126c1dd3f71
Werner Van Geit <werner.vangeit@epfl.ch>
2016-11-14 16:50:27 +0100

    Added matplotlib dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 setuptools.setup(
     name="bluepymm",
     version='0.1',
-    install_requires=['sh', 'bluepyopt'],
+    install_requires=['sh', 'bluepyopt', 'matplotlib'],
     packages=['bluepymm', 'bluepymm/megate'],
     author="BlueBrain Project, EPFL",
     author_email="werner.vangeit@epfl.ch",
