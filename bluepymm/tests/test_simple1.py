cd278c04596a505b71d303024ec8e13c5f584250
Werner Van Geit <werner.vangeit@epfl.ch>
2017-04-24 17:08:18 +0200

    Add test to check megating output files of simple1 test

--- bluepymm/tests/test_simple1.py
+++ bluepymm/tests/test_simple1.py
@@ -3,6 +3,10 @@
 # Copyright BBP/EPFL 2017; All rights reserved.
 # Do not distribute without further notice.
 
+import filecmp
+
+import nose.tools as nt
+
 import bluepymm as pymm
 import bluepymm.megate as megate
 
@@ -15,8 +19,15 @@ def test_simple1():
         args = pymm.parse_args(args_list)
         pymm.run(args)
 
-    with pymm.tools.cd('examples/simple1'):
         args_list = ['simple1_megate_conf.json']
         args = megate.parse_args(args_list)
         megate.run(args)
 
+        matches = filecmp.cmpfiles(
+                'output_megate_expected', 'output_megate',
+                ['combo_model.csv', 'extNeuronDB.dat'])
+
+        if len(matches[0]) != 2:
+            print('Mismatch in files: %s' % matches[1])
+
+        nt.assert_equal(len(matches[0]), 2)
