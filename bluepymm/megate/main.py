b56847b4d9e83ce94772f77f3da3262a65ecf42c
Liesbeth Vanherpe <liesbeth.vanherpe@epfl.ch>
2017-04-28 16:10:37 +0200

    Updated report: titles and labels.

--- bluepymm/megate/main.py
+++ bluepymm/megate/main.py
@@ -183,6 +183,19 @@ def plot_megate_thresholds(megate_thresholds, pp):
     plt.savefig(pp, format='pdf', bbox_inches='tight')
 
 
+def plot_stacked_bars(data, xlabel, ylabel, title, color_map, pp):
+    ax = data.plot(kind='barh', figsize=FIGSIZE, stacked=True,
+                   color=color_map)
+    ax.get_xaxis().set_major_locator(MaxNLocator(integer=True))
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.tight_layout()
+    plt.legend(loc='upper right')
+    plt.savefig(pp, format='pdf', bbox_inches='tight')
+    plt.close()
+
+
 def plot_morphs_per_feature_for_emodel(emodel, megate_scores,
                                        emodel_score_values, pp):
     """Display number of morphs matches per feature for a given emodel"""
@@ -191,15 +204,13 @@ def plot_morphs_per_feature_for_emodel(emodel, megate_scores,
     sums['passed'] = megate_scores.sum(axis=0)
     sums['failed'] = len(emodel_score_values) - sums['passed']
 
-    ax = sums.plot(kind='barh', figsize=FIGSIZE, stacked=True,
-                   color=[BLUE, RED])
-    ax.get_xaxis().set_major_locator(plt.MaxNLocator(integer=True))
-
-    plt.xlabel('# morphologies')
-    plt.title(emodel)
-    plt.tight_layout()
-    plt.savefig(pp, format='pdf', bbox_inches='tight')
-    plt.close()
+    plot_stacked_bars(sums,
+                      '# morphologies',
+                      '',
+                      '{}: number of tested morphologies per feature'.format(emodel),
+                      [BLUE, RED],
+                      pp,
+                     )
 
 
 def plot_morphs_per_mtype_for_emodel(emodel, mtypes, megate_scores, pp):
@@ -213,16 +224,20 @@ def plot_morphs_per_mtype_for_emodel(emodel, mtypes, megate_scores, pp):
         sums.ix[mtype, 'failed'] = (len(megate_scores_mtype)
                                     - sums.ix[mtype, 'passed'])
 
-    if len(sums) > 0:
-        ax = sums.plot(kind='barh', figsize=FIGSIZE, stacked=True,
-                       color=[BLUE, RED])
-        ax.get_xaxis().set_major_locator(plt.MaxNLocator(integer=True))
+    plot_stacked_bars(sums,
+                      '# morphologies',
+                      '',
+                      '{}: number of tested morphologies per m-type'.format(emodel),
+                      [BLUE, RED],
+                      pp,
+                     )
 
-    plt.xlabel('# morphs')
-    plt.title(emodel)
-    plt.tight_layout()
-    plt.savefig(pp, format='pdf', bbox_inches='tight')
-    plt.close()
+
+def create_morphology_label(data_frame):
+    morph = data_frame.iloc[0]['morph_name']
+    mtype = data_frame.iloc[0]['fullmtype']
+    etype = data_frame.iloc[0]['etype']
+    return '{} ({}, {})'.format(morph, mtype, etype)
 
 
 def plot_emodels_per_morphology(data, final_db, pp):
@@ -232,37 +247,34 @@ def plot_emodels_per_morphology(data, final_db, pp):
     non_exemplars = data[data['is_exemplar'] == 0]
     for morph_name in non_exemplars['morph_name'].unique():
         nb_matches = len(final_db[final_db['morph_name'] == morph_name])
+        non_exemplars_morph = non_exemplars[
+            non_exemplars['morph_name'] == morph_name]
         nb_errors = len(
-            non_exemplars[
-                (non_exemplars['morph_name'] == morph_name) & (
-                    non_exemplars['exception'].notnull())])
-        nb_combos = len(
-            non_exemplars[
-                non_exemplars['morph_name'] == morph_name])
-        sums.ix[morph_name, 'passed'] = nb_matches
-        sums.ix[morph_name, 'error'] = nb_errors
-        sums.ix[morph_name, 'failed'] = nb_combos - nb_matches - nb_errors
+            non_exemplars_morph[non_exemplars_morph['exception'].notnull()])
+        nb_combos = len(non_exemplars_morph)
 
-    ax = sums.plot(kind='barh', figsize=FIGSIZE, stacked=True,
-                   color=[BLUE, YELLOW, RED])
-    ax.get_xaxis().set_major_locator(plt.MaxNLocator(integer=True))
+        label = create_morphology_label(non_exemplars_morph)
+        sums.ix[label, 'passed'] = nb_matches
+        sums.ix[label, 'error'] = nb_errors
+        sums.ix[label, 'failed'] = nb_combos - nb_matches - nb_errors
 
-    plt.xlabel('# tested e-models')
-    plt.ylabel('Morphology name')
-    plt.title('Number of tested e-models for each morphology')
-    plt.tight_layout()
-    plt.savefig(pp, format='pdf', bbox_inches='tight')
-    plt.close()
+    plot_stacked_bars(sums,
+                      '# tested e-models',
+                      'Morphology name',
+                      'Number of tested e-models for each morphology',
+                      [BLUE, YELLOW, RED],
+                      pp,
+                     )
 
 
 def plot_emodels_per_metype(data, final_db, pp):
     """Display result of tested e-model / morphology combinations per me-type"""
 
-    sums = pandas.DataFrame()
-    non_exemplars = data[data['is_exemplar'] == 0].copy()
+    data['metype'] = data.apply(
+            lambda x: '%s_%s' % (x['etype'], x['fullmtype']), axis=1)
 
-    non_exemplars['metype'] = non_exemplars.apply(
-        lambda x: '%s_%s' % (x['etype'], x['fullmtype']), axis=1)
+    sums = pandas.DataFrame()
+    non_exemplars = data[data['is_exemplar'] == 0]
     for metype in non_exemplars['metype'].unique():
         nb_matches = len(final_db[final_db['combo_name'].str.contains(metype)])
         nb_errors = len(
@@ -274,16 +286,14 @@ def plot_emodels_per_metype(data, final_db, pp):
         sums.ix[metype, 'error'] = nb_errors
         sums.ix[metype, 'failed'] = nb_combos - nb_matches - nb_errors
 
-    ax = sums.plot(kind='barh', figsize=FIGSIZE, stacked=True,
-                   color=[BLUE, YELLOW, RED])
-    ax.get_xaxis().set_major_locator(plt.MaxNLocator(integer=True))
-
-    plt.xlabel('# tested (e-model, morphology) combinations')
-    plt.ylabel('me-type')
-    plt.title('Number of tested (e-model, morphology) combinations per me-type')
-    plt.tight_layout()
-    plt.savefig(pp, format='pdf', bbox_inches='tight')
-    plt.close()
+    plot_stacked_bars(sums,
+                      '# tested (e-model, morphology) combinations',
+                      'me-type',
+                      'Number of tested (e-model, morphology) combinations per'
+                         ' me-type',
+                      [BLUE, YELLOW, RED],
+                      pp,
+                     )
 
 
 def check_opt_scores(emodel, scores):
