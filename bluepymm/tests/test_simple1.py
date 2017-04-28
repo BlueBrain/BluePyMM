39e41a6a19ecefe9221eed9ffacf78db828bfe76
Werner Van Geit <werner.vangeit@epfl.ch>
2017-04-28 13:14:09 +0200

    Compare output db in test

--- bluepymm/tests/test_simple1.py
+++ bluepymm/tests/test_simple1.py
@@ -15,19 +15,38 @@ def test_simple1():
     """simple1: test"""
 
     with pymm.tools.cd('examples/simple1'):
+
+        # Run mm
         args_list = ['simple1_conf.json']
         args = pymm.parse_args(args_list)
         pymm.run(args)
 
+        # Test output db
+        import pandas
+        import sqlite3
+
+        scores_sqlite_filename = 'output/scores.sqlite'
+        exp_scores_sqlite_filename = 'output_expected/scores.sqlite'
+        with sqlite3.connect(scores_sqlite_filename) as conn:
+            scores = pandas.read_sql('SELECT * FROM scores', conn)
+
+        with sqlite3.connect(exp_scores_sqlite_filename) as conn:
+            exp_scores = pandas.read_sql('SELECT * FROM scores', conn)
+
+        nt.assert_true(scores.equals(exp_scores))
+
+        # Run megate
         args_list = ['simple1_megate_conf.json']
         args = megate.parse_args(args_list)
         megate.run(args)
 
+        # Test megate output
         matches = filecmp.cmpfiles(
-                'output_megate_expected', 'output_megate',
-                ['combo_model.csv', 'extNeuronDB.dat'])
+            'output_megate_expected', 'output_megate',
+            ['combo_model.csv', 'extNeuronDB.dat'])
 
         if len(matches[0]) != 2:
             print('Mismatch in files: %s' % matches[1])
 
         nt.assert_equal(len(matches[0]), 2)
+
