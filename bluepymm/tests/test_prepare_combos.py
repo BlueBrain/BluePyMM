e251ecdbfc8992282fdd01ed2d31b0ca91194550
Liesbeth Vanherpe <liesbeth.vanherpe@epfl.ch>
2017-05-22 16:17:20 +0200

    Improve prepare combos testing.

--- bluepymm/tests/test_prepare_combos.py
+++ bluepymm/tests/test_prepare_combos.py
@@ -25,9 +25,26 @@ def _verify_main_output(scores_db, emodels_hoc_dir):
         nt.assert_equal(hoc_file[-4:], '.hoc')
 
 
-def test_main():
+def test_main_from_dir():
     test_dir = 'examples/simple1'
-    test_config = 'simple1_conf.json'
+    test_config = 'simple1_conf_prepare.json'
+
+    with tools.cd(test_dir):
+        # Make sure the output directories are clean
+        _clear_main_output()
+
+        # Run combination preparation
+        args_list = [test_config]
+        prepare_combos.main(args_list)
+
+        # Test output
+        config = tools.load_json(test_config)
+        _verify_main_output(config["scores_db"], config["emodels_hoc_dir"])
+
+
+def test_main_from_git_repo():
+    test_dir = 'examples/simple1'
+    test_config = 'simple1_conf_prepare_git.json'
 
     with tools.cd(test_dir):
         # Make sure the output directories are clean
