7e30bb14d32a8f3252672923b130d0bdcb55d6b2
Werner Van Geit <werner.vangeit@epfl.ch>
2017-07-05 11:35:01 +0200

    Move tmp files to tmp folder

--- bluepymm/tests/test_calculate_scores.py
+++ bluepymm/tests/test_calculate_scores.py
@@ -15,6 +15,7 @@ import bluepymm.run_combos as run_combos
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 TEST_DIR = os.path.join(BASE_DIR, 'examples/simple1')
+TMP_DIR = os.path.join(BASE_DIR, 'tmp/')
 
 
 @attr('unit')
@@ -58,10 +59,10 @@ def test_run_emodel_morph():
     nt.assert_dict_equal(ret[1], expected_extra_values)
 
 
-def _write_test_scores_database(row, filename):
+def _write_test_scores_database(row, testsqlite_filename):
     """Helper function to create test scores database."""
     df = pandas.DataFrame(row, index=[0])
-    with sqlite3.connect(filename) as conn:
+    with sqlite3.connect(testsqlite_filename) as conn:
         df.to_sql('scores', conn, if_exists='replace')
 
 
@@ -69,7 +70,7 @@ def _write_test_scores_database(row, filename):
 def test_create_arg_list():
     """run_combos.calculate_scores: test create_arg_list."""
     # write database
-    filename = 'test.sqlite'
+    testsqlite_filename = os.path.join(TMP_DIR, 'test1.sqlite')
     index = 0
     morph_name = 'morph'
     morph_dir = 'morph_dir'
@@ -86,7 +87,7 @@ def test_create_arg_list():
            'emodel': emodel,
            'original_emodel': emodel,
            'to_run': 1}
-    _write_test_scores_database(row, filename)
+    _write_test_scores_database(row, testsqlite_filename)
 
     # extra input parameters
     emodel_dirs = {emodel: 'emodel_dirs'}
@@ -94,7 +95,7 @@ def test_create_arg_list():
     final_dict = {emodel: {'params': params}}
 
     ret = run_combos.calculate_scores.create_arg_list(
-        filename,
+        testsqlite_filename,
         emodel_dirs,
         final_dict)
 
@@ -107,15 +108,12 @@ def test_create_arg_list():
                      os.path.abspath(morph_path))]
     nt.assert_list_equal(ret, expected_ret)
 
-    # remove database
-    os.remove(filename)
-
 
 @attr('unit')
 def test_create_arg_list_exception():
     """run_combos.calculate_scores: test create_arg_list for ValueError."""
     # write database
-    filename = 'test.sqlite'
+    testsqlite_filename = os.path.join(TMP_DIR, 'test2.sqlite')
     index = 0
     morph_name = 'morph'
     morph_dir = 'morph_dir'
@@ -132,7 +130,7 @@ def test_create_arg_list_exception():
            'emodel': emodel,
            'original_emodel': emodel,
            'to_run': 1}
-    _write_test_scores_database(row, filename)
+    _write_test_scores_database(row, testsqlite_filename)
 
     # extra input parameters
     emodel_dirs = {emodel: 'emodel_dirs'}
@@ -143,12 +141,9 @@ def test_create_arg_list_exception():
     nt.assert_raises(
         ValueError,
         run_combos.calculate_scores.create_arg_list,
-        filename,
+        testsqlite_filename,
         emodel_dirs,
         final_dict)
-
-    # remove database
-    os.remove(filename)
 
 
 def _dict_factory(cursor, row):
@@ -163,12 +158,12 @@ def _dict_factory(cursor, row):
 def test_save_scores():
     """run_combos.calculate_scores: test save_scores"""
     # create test database with single entry 'row'
-    filename = 'test.sqlite'
+    testsqlite_filename = os.path.join(TMP_DIR, 'test3.sqlite')
     row = {'scores': None,
            'extra_values': None,
            'exception': None,
            'to_run': True}
-    _write_test_scores_database(row, filename)
+    _write_test_scores_database(row, testsqlite_filename)
 
     # process database
     uid = 0
@@ -176,7 +171,7 @@ def test_save_scores():
     extra_values = {'extra': 2}
     exception = 'exception'
     run_combos.calculate_scores.save_scores(
-        filename,
+        testsqlite_filename,
         uid,
         scores,
         extra_values,
@@ -188,7 +183,7 @@ def test_save_scores():
                        'extra_values': json.dumps(extra_values),
                        'exception': exception,
                        'to_run': 0}  # False
-    with sqlite3.connect(filename) as scores_db:
+    with sqlite3.connect(testsqlite_filename) as scores_db:
         scores_db.row_factory = _dict_factory
         scores_cursor = scores_db.execute('SELECT * FROM scores')
         db_row = scores_cursor.fetchall()[0]
@@ -198,7 +193,7 @@ def test_save_scores():
     nt.assert_raises(
         ValueError,
         run_combos.calculate_scores.save_scores,
-        filename,
+        testsqlite_filename,
         uid,
         scores,
         extra_values,
