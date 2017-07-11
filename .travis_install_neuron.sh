aac16f1a1ea2f9df2626e8c29b730743e6693d4b
Werner Van Geit <werner.vangeit@gmail.com>
2017-07-11 11:25:16 +0200

    Switch tox neuron (#99)
    * Split unit and !unit tests in tox
    
    * Try enabling neuron building in tox
    
    * Enable neuron compilation by tox
    
    * Remove neuron install from travis
    
    * Switch to py3 in tox instead of 36
    
    * Save .tox in travis cache
    
    * Add lib64 to pythonpath in tox.ini
    
    * cache .tox instead of home/.tox
    
    * Cache $TRAVIS_BUILD_DIR/.tox
    
    * Make symbolic from every neuron bin in tox
    
    * Fix find command in tox.ini

--- .travis_install_neuron.sh
+++ .travis_install_neuron.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 SRC_DIR=$1
 INSTALL_DIR=$2
 
 if [ ! -e ${INSTALL_DIR}/.install_finished ]
 then
+    echo 'Neuron was not fully installed in previous build, installing ...'
     mkdir -p ${SRC_DIR}
     cd ${SRC_DIR}
-    wget http://www.neuron.yale.edu/ftp/neuron/versions/v7.4/nrn-7.4.tar.gz
-    tar xzvf nrn-7.4.tar.gz
+    echo "Downloading NEURON 7.4 ..."
+    wget -q http://www.neuron.yale.edu/ftp/neuron/versions/v7.4/nrn-7.4.tar.gz
+    tar xzf nrn-7.4.tar.gz
     cd nrn-7.4
-    ./configure --prefix=${INSTALL_DIR} --without-x --with-nrnpython have_cython=no BUILD_RX3D=0
-    make -j4 install
+    echo "Configuring NEURON ..."
+    ./configure --prefix=${INSTALL_DIR} --without-x --with-nrnpython --disable-rx3d >configure.log 2>&1
+    echo "Installing NEURON ..."
+    make -j4 install >makeinstall.log 2>&1
 
     export PATH="${INSTALL_DIR}/x86_64/bin":${PATH}
     export PYTHONPATH="${INSTALL_DIR}/lib/python":${PYTHONPATH}
 
-    python -c 'import neuron'
+    echo "Testing NEURON import ...."
+    python -c 'import neuron' >testimport.log 2>&1
 
     touch -f ${INSTALL_DIR}/.install_finished
+    echo "NEURON successfully installed"
 else
-    echo 'Neuron was fully installed in previous build, not rebuilding'
+    echo 'Neuron was successfully installed in previous build, not rebuilding'
 fi
