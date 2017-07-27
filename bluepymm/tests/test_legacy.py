5fe3aeb74365f42b0825c07d9e7424a6a23c651d
Liesbeth Vanherpe <liesbeth.vanherpe@epfl.ch>
2017-07-27 11:30:03 +0200

    Bug fixing in tests

--- bluepymm/tests/test_legacy.py
+++ bluepymm/tests/test_legacy.py
@@ -27,23 +27,33 @@ from nose.plugins.attrib import attr
 import nose.tools as nt
 
 import bluepymm
-from bluepymm import tools
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 TEST_DATA_DIR = os.path.join(BASE_DIR, 'examples/simple1')
-TMP_DIR = os.path.join(BASE_DIR, 'tmp/legacy')
+TMP_DIR = os.path.join(BASE_DIR, 'tmp/test_legacy')
+
+
+def _new_prepare_json(original_filename, test_dir):
+    """Helper function to prepare new configuration file for prepare_combos."""
+    config = bluepymm.tools.load_json(original_filename)
+    config['tmp_dir'] = os.path.join(test_dir, 'tmp')
+    config['output_dir'] = os.path.join(test_dir, 'output')
+    config['scores_db'] = os.path.join(config['output_dir'], 'scores.sqlite')
+    config['emodels_hoc_dir'] = os.path.join(config['output_dir'],
+                                             'emodels_hoc')
+    return bluepymm.tools.write_json(test_dir, original_filename, config)
 
 
 def _prepare_config_jsons(prepare_config_template_filename,
                           hoc_config_template_filename):
     """Helper function to prepare configuration .json files."""
     # load json files
-    prepare_config = tools.load_json(prepare_config_template_filename)
-    hoc_config = tools.load_json(hoc_config_template_filename)
+    prepare_config = bluepymm.tools.load_json(prepare_config_template_filename)
+    hoc_config = bluepymm.tools.load_json(hoc_config_template_filename)
 
     # use TMP_DIR for output
-    tools.makedirs(TMP_DIR)
+    bluepymm.tools.makedirs(TMP_DIR)
     prepare_config['tmp_dir'] = os.path.abspath(os.path.join(TMP_DIR, 'tmp'))
     prepare_config['output_dir'] = os.path.abspath(os.path.join(TMP_DIR,
                                                                 'output'))
@@ -53,9 +63,9 @@ def _prepare_config_jsons(prepare_config_template_filename,
                                                                 'hoc_output'))
 
     # write out changes to TMP_DIR
-    prepare_config_path = tools.write_json(
+    prepare_config_path = bluepymm.tools.write_json(
         TMP_DIR, prepare_config_template_filename, prepare_config)
-    hoc_config_path = tools.write_json(
+    hoc_config_path = bluepymm.tools.write_json(
         TMP_DIR, hoc_config_template_filename, hoc_config)
 
     return prepare_config_path, hoc_config_path
@@ -70,11 +80,16 @@ def test_get_parser():
 @attr('unit')
 def test_add_full_paths():
     """bluepymm.legacy: test add_full_paths"""
-    test_dict = {'test': 'tmp/legacy', 'int': 1}
+    # input parameters
+    test_dict = {'test': 'tmp/test_legacy/test_add_full_paths', 'int': 1}
     directory = BASE_DIR
 
-    tools.makedirs(TMP_DIR)
-    expected_ret = {'test': TMP_DIR, 'int': 1}
+    # prepare test data
+    test_dir = os.path.join(TMP_DIR, 'test_add_full_paths')
+    bluepymm.tools.makedirs(test_dir)
+
+    # run function and verify output
+    expected_ret = {'test': test_dir, 'int': 1}
     ret = bluepymm.legacy.create_hoc_files.add_full_paths(test_dict, directory)
     nt.assert_dict_equal(ret, expected_ret)
 
@@ -123,32 +138,99 @@ def test_load_combinations_dict():
 @attr('unit')
 def test_run_create_and_write_hoc_file():
     """bluepymm.legacy: test run_create_and_write_hoc_file"""
+    # create test directory, where output of this test will be created
+    test_dir = os.path.join(TMP_DIR, 'test_run_create_and_write_hoc_file')
+    bluepymm.tools.makedirs(test_dir)
+
+    # input parameters
+    prepare_conf_template = 'simple1_conf_prepare.json'
+
     emodel = 'emodel1'
-    emodel_dir = './data/emodels_dir/subdir/'
-    hoc_dir = './output/emodels_hoc'
+    emodel_dir = os.path.join(test_dir, 'tmp', 'emodels', emodel)
+    hoc_dir = os.path.join(test_dir, 'emodels_hoc')
     emodel_parameters = {'cm': 1.0}
     template = 'cell_template.jinja2'
     template_dir = '.'
     morph_path = 'morph.asc'
     model_name = 'test'
 
-    with tools.cd(TEST_DATA_DIR):
-        tools.makedirs(hoc_dir)
+    with bluepymm.tools.cd(TEST_DATA_DIR):
+        # prepare input files
+        prepare_conf_path = _new_prepare_json(prepare_conf_template, test_dir)
+        bluepymm.prepare_combos.main.prepare_combos(prepare_conf_path, False)
 
+        # create output directory and run function
+        bluepymm.tools.makedirs(hoc_dir)
         bluepymm.legacy.create_hoc_files.run_create_and_write_hoc_file(
             emodel, emodel_dir, hoc_dir, emodel_parameters, template,
             template_dir, morph_path, model_name)
 
-        # TODO: test hoc file contents
-        template_name = model_name or emodel
-        hoc_filename = '{}.hoc'.format(template_name)
+        # verify output directory. TODO: test hoc file contents
+        expected_nb_files = 1
+        list_of_files = os.listdir(hoc_dir)
+        nt.assert_equal(len(list_of_files), expected_nb_files)
+        hoc_filename = '{}.hoc'.format(model_name)
+        hoc_path = os.path.join(hoc_dir, hoc_filename)
+        nt.assert_true(os.path.isfile(hoc_path))
+
+
+@attr('unit')
+def test_create_hoc_files():
+    """bluepymm.legacy: test create_hoc_files"""
+    # create test directory, where output of this test will be created
+    test_dir = os.path.join(TMP_DIR, 'test_create_hoc_files')
+    bluepymm.tools.makedirs(test_dir)
+
+    # input parameters
+    prepare_conf_template = 'simple1_conf_prepare.json'
+
+    model_name = 'combo1'
+    combo_dict = {model_name: {'morph_name': 'morph1',
+                               'layer': '1',
+                               'fullmtype': 'mtype1',
+                               'etype': 'etype1',
+                               'emodel': 'emodel1',
+                               'combo_name': 'combo1',
+                               'threshold_current': '',
+                               'holding_current': '',
+                               },
+                  }
+    emodels_dir = os.path.join(test_dir, 'tmp', 'emodels')
+    final_dict = {'emodel1': {'main_path': '.',
+                              'seed': 2,
+                              'rank': 0,
+                              'notes': '',
+                              'branch': 'emodel1',
+                              'params': {'cm': 1.0},
+                              'fitness': {'Step1.SpikeCount': 20.0},
+                              'score': 104.72906197480131,
+                              'morph_path': 'morphologies/morph1.asc'}
+                  }
+    template = 'cell_template.jinja2'
+    hoc_dir = os.path.join(test_dir, 'emodels_hoc')
+
+    with bluepymm.tools.cd(TEST_DATA_DIR):
+        # prepare input files
+        prepare_conf_path = _new_prepare_json(prepare_conf_template, test_dir)
+        bluepymm.prepare_combos.main.prepare_combos(prepare_conf_path, False)
+
+        # create output directory and run function
+        bluepymm.tools.makedirs(hoc_dir)
+        bluepymm.legacy.create_hoc_files.create_hoc_files(
+            combo_dict, emodels_dir, final_dict, template, hoc_dir)
+
+        # verify output directory. TODO: test hoc file contents
+        expected_nb_files = 1
+        list_of_files = os.listdir(hoc_dir)
+        nt.assert_equal(len(list_of_files), expected_nb_files)
+        hoc_filename = '{}.hoc'.format(model_name)
         hoc_path = os.path.join(hoc_dir, hoc_filename)
         nt.assert_true(os.path.isfile(hoc_path))
 
 
 def _verify_output(hoc_config_path):
     """Helper function to verify output"""
-    hoc_config = tools.load_json(hoc_config_path)
+    hoc_config = bluepymm.tools.load_json(hoc_config_path)
 
     # verify .hoc-files existence - TODO: verify content
     nt.assert_true(os.path.isdir(hoc_config['hoc_output_dir']))
@@ -164,7 +246,7 @@ def test_create_hoc_files():
     """bluepymm.legacy: test creation legacy .hoc files for example simple1"""
     prepare_config_template_filename = 'simple1_conf_prepare.json'
     hoc_config_template_filename = 'simple1_conf_hoc.json'
-    with tools.cd(TEST_DATA_DIR):
+    with bluepymm.tools.cd(TEST_DATA_DIR):
         prepare_config_path, hoc_config_path = _prepare_config_jsons(
             prepare_config_template_filename, hoc_config_template_filename)
 
