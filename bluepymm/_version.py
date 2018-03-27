dd4c90b31b24db22080ecc4bd6b337a1c4dfac2e
Werner Van Geit <werner.vangeit@epfl.ch>
2018-03-27 11:38:16 +0200

    Fix version file

--- bluepymm/_version.py
+++ bluepymm/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg.style = "pep440-minor"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = ""
-    cfg.versionfile_source = "bluepyopt/_version.py"
+    cfg.versionfile_source = "bluepymm/_version.py"
     cfg.verbose = False
     return cfg
 
