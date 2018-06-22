7a1c0ec22eefe661d4b3f6ce93b4f162e19f31fd
Werner Van Geit <werner.vangeit@epfl.ch>
2018-06-22 16:09:54 +0200

    Show all output during neuron install for debugging

--- .travis_install_neuron.sh
+++ .travis_install_neuron.sh
@@ -11,16 +11,21 @@ then
     mkdir -p ${SRC_DIR}
     cd ${SRC_DIR}
     echo "Downloading NEURON ..."
-	git clone --depth 1 https://github.com/nrnhines/nrn.git >download.log 2>&1
+	# git clone --depth 1 https://github.com/nrnhines/nrn.git >download.log 2>&1
+	git clone --depth 1 https://github.com/nrnhines/nrn.git
 	cd nrn
     echo "Preparing NEURON ..."
-	./build.sh >prepare.log 2>&1
+	# ./build.sh >prepare.log 2>&1
+	./build.sh
     echo "Configuring NEURON ..."
-    ./configure --prefix=${INSTALL_DIR} --without-x --with-nrnpython=python --disable-rx3d >configure.log 2>&1
+    # ./configure --prefix=${INSTALL_DIR} --without-x --with-nrnpython=python --disable-rx3d >configure.log 2>&1
+    ./configure --prefix=${INSTALL_DIR} --without-x --with-nrnpython=python --disable-rx3d
     echo "Building NEURON ..."
-    make -j4 >make.log 2>&1
+    # make -j4 >make.log 2>&1
+    make -j4
     echo "Installing NEURON ..."
-    make -j4 install >install.log 2>&1
+    # make -j4 install >install.log 2>&1
+    make -j4 install
 
     export PATH="${INSTALL_DIR}/x86_64/bin":${PATH}
     export PYTHONPATH="${INSTALL_DIR}/lib/python":${PYTHONPATH}
