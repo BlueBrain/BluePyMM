a3d3276533444b5dd2b79d12f20e1d870c82e2bb
Anil Tuncel <anil.tuncel@epfl.ch>
2020-05-07 14:19:33 +0200

    evaluator.create returns a multievaluator

--- bluepymm/tests/examples/simple1/data/emodels_dir/subdir/setup/evaluator.py
+++ bluepymm/tests/examples/simple1/data/emodels_dir/subdir/setup/evaluator.py
@@ -1,6 +1,109 @@
 """Run simple cell optimisation"""
 
 import bluepyopt.ephys as ephys
+import bluepyopt as bpopt
+
+class MultiEvaluator(bpopt.evaluators.Evaluator):
+
+    """Multiple cell evaluator"""
+
+    def __init__(
+            self,
+            evaluators=None,
+            sim=None,
+            ):
+        """Constructor
+
+        Args:
+            evaluators (list): list of CellModel evaluators
+        """
+
+        self.sim = sim
+        self.evaluators = evaluators
+        objectives = []
+        # loop objectives for all evaluators, rename based on evaluators
+        for i, evaluator in enumerate(self.evaluators):
+            for objective in evaluator.objectives:
+                objectives.append(objective)
+
+        # these are identical for all models. Better solution available?
+        self.param_names = self.evaluators[0].param_names
+        params = self.evaluators[0].cell_model.params_by_names(self.param_names)
+
+        super(MultiEvaluator, self).__init__(
+            objectives,
+            params)
+
+    def param_dict(self, param_array):
+        """Convert param_array in param_dict"""
+        param_dict = {}
+        for param_name, param_value in \
+                zip(self.param_names, param_array):
+            param_dict[param_name] = param_value
+
+        return param_dict
+
+    def objective_dict(self, objective_array):
+        """Convert objective_array in objective_dict"""
+        objective_dict = {}
+        objective_names = [objective.name
+                           for objective in self.objectives]
+
+        if len(objective_names) != len(objective_array):
+            raise Exception(
+                'MultiEvaluator: list given to objective_dict() '
+                'has wrong number of objectives')
+
+        for objective_name, objective_value in \
+                zip(objective_names, objective_array):
+            objective_dict[objective_name] = objective_value
+
+        return objective_dict
+
+    def objective_list(self, objective_dict):
+        """Convert objective_dict in objective_list"""
+        objective_list = []
+        objective_names = [objective.name
+                           for objective in self.objectives]
+        for objective_name in objective_names:
+            objective_list.append(objective_dict[objective_name])
+
+        return objective_list
+
+    def evaluate_with_dicts(self, param_dict=None):
+        """Run evaluation with dict as input and output"""
+
+        scores = {}
+        for evaluator in self.evaluators:
+            score = evaluator.evaluate_with_dicts(param_dict=param_dict)
+            scores.update(score)
+
+        return scores
+
+    def evaluate_with_lists(self, param_list=None):
+        """Run evaluation with lists as input and outputs"""
+
+        param_dict = self.param_dict(param_list)
+
+        obj_dict = self.evaluate_with_dicts(param_dict=param_dict)
+
+        return self.objective_list(obj_dict)
+
+    def evaluate(self, param_list=None):
+        """Run evaluation with lists as input and outputs"""
+
+        return self.evaluate_with_lists(param_list)
+
+    def __str__(self):
+
+        content = 'multi cell evaluator:\n'
+
+        content += '  evaluators:\n'
+        for evaluator in self.evaluators:
+            content += '    %s\n' % str(evaluator)
+
+        return content
+
 
 
 def create(etype):
@@ -80,4 +183,10 @@ def create(etype):
         fitness_calculator=score_calc,
         sim=nrn)
 
-    return cell_evaluator
+    all_cell_evaluators = [cell_evaluator]
+
+    multi_eval = MultiEvaluator(
+                        evaluators=all_cell_evaluators,
+                        sim=nrn)
+
+    return multi_eval
