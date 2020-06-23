4f75e0e0a176814589f6b36574843d16b825ba53
Anil Tuncel <anil.tuncel@epfl.ch>
2020-06-23 17:16:27 +0200

    added test for yaml parser

--- bluepymm/tests/test_parse_files.py
+++ bluepymm/tests/test_parse_files.py
@@ -149,6 +149,24 @@ def test_read_mm_recipe_xml():
 
 
 @attr('unit')
+def test_read_mm_recipe_yaml():
+    """bluepymm.prepare_combos.parse_files: test read_mm_recipe with a yaml
+    recipe from test example "simple1".
+    """
+    recipe_filename = os.path.join(
+        BASE_DIR,
+        'examples/simple1/data/simple1_recipe.yaml')
+    expected_records = [("1", "mtype1", "etype1"),
+                        ("1", "mtype1", "etype2"),
+                        ("1", "mtype2", "etype1"),
+                        ("2", "mtype1", "etype2"), ]
+    expected_df = pandas.DataFrame(expected_records,
+                                   columns=["layer", "fullmtype", "etype"])
+    df = parse_files.read_mm_recipe(recipe_filename)
+    pandas.testing.assert_frame_equal(df, expected_df)
+
+
+@attr('unit')
 def test_read_morph_records():
     """bluepymm.prepare_combos.parse_files: test read_morph_records.
     """
