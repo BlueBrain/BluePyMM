27b60622b847f9ba3f048001f8b1075c4b73e5b3
Anil Tuncel <anil.tuncel@epfl.ch>
2020-06-23 17:35:37 +0200

    added pyyaml to setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     install_requires=['sh', 'bluepyopt', 'matplotlib', 'pandas', 'numpy',
-                      'ipyparallel', 'lxml', 'h5py'],
+                      'ipyparallel', 'lxml', 'h5py', 'pyyaml'],
     packages=setuptools.find_packages(exclude=('notebook',)),
     author="BlueBrain Project, EPFL",
     author_email="werner.vangeit@epfl.ch",
