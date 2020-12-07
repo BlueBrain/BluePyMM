88ad1cf9d1055250f89f00e131bdbeeb08942645
Anil Tuncel <anil.tuncel@epfl.ch>
2020-12-07 16:02:40 +0100

    test_calculate_scores: explicitly call ipcluster stop
    This ipyparallel issue recently started causing Jenkins CI plan to fail

--- bluepymm/tests/test_calculate_scores.py
+++ bluepymm/tests/test_calculate_scores.py
@@ -389,6 +389,8 @@ def test_calculate_scores():
 
         if use_ipyp:
             ip_proc.terminate()
+            subprocess.run(["ipcluster", "stop"], capture_output=True,
+                           check=True)
 
         # verify database
         scores = {'Step1.SpikeCount': 20.0}
